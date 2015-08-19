afe88d4cd9521941a6376408123430f847c0eb0b
Miguel Fernandes <it.miguelfernandes@gmail.com>
2015-08-19 17:47:16 +0100

    Correct model error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ActiveRecord::Base
          :validatable
 
   validates :first_name, presence: true,
-                         length: { maximum: 30 },
+                         length: { maximum: 30 }
 
   validates :last_name, presence: true,
-                        length: { maximum: 30 },
+                        length: { maximum: 30 }
 
   validates :email, presence: true,
                     format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,11 +3,9 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   should validate_presence_of(:first_name)
   should validate_length_of(:first_name).is_at_most(30)
-  should_not allow_value('1miguel', 'miguel duarte', '@miguel').for(:first_name)
 
   should validate_presence_of(:last_name)
   should validate_length_of(:last_name).is_at_most(30)
-  should_not allow_value('1fernandes', 'miguel fernandes', '@fernandes').for(:last_name)
 
   should validate_presence_of(:email)
   should_not allow_value('test@test', 'test@', '@test.com').for(:email)
