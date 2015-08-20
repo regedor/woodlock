b3834e371cd8ea11ea1a966d99c0e437a64975cf
Miguel Fernandes <it.miguelfernandes@gmail.com>
2015-08-20 23:17:50 +0100

    Append viewport zoom meta with js and change sign in social to display block

--- app/assets/javascripts/woodlock.js
+++ app/assets/javascripts/woodlock.js
@@ -1,3 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function() {
+  $("head").append("<meta name='viewport' content='width=device-width, initial-scale=1'>");
+});
