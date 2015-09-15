171796ec6980e1d9d965e8aefbbeb6a8f9f54e8b
Miguel Fernandes <it.miguelfernandes@gmail.com>
2015-09-15 16:04:13 +0100

    Fix Google Analytics script.

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -1,7 +1,6 @@
 app_env_vars = File.join(Rails.root, 'config', 'initializers', 'app_env_vars.rb')
 load(app_env_vars) if File.exists?(app_env_vars)
 
-
 Rails.application.configure do
   
   # Configurations from engine
