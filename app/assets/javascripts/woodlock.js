4369374fd8f7179d9f86b28d138f917fc84c8154
Miguel Fernandes <it.miguelfernandes@gmail.com>
2015-09-15 14:58:42 +0100

    Add Google Analytics.

--- app/assets/javascripts/woodlock.js
+++
@@ -1,6 +0,0 @@
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
-$(document).ready(function() {
-  $("head").append("<meta name='viewport' content='width=device-width, initial-scale=1'>");
-});
