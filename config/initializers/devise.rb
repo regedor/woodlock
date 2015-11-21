384f59ce8b771d2ff2cfb336197daf43e8c0679d
Miguel Fernandes <it.miguelfernandes@gmail.com>
2015-11-21 16:59:33 +0000

    Add repo scope to config.omniauth .

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -6,7 +6,7 @@ Devise.setup do |config|
   require "omniauth-github"
   config.omniauth :facebook, ENV['OMNIAUTH_FACEBOOK_APP_ID'], ENV['OMNIAUTH_FACEBOOK_APP_SECRET']
   config.omniauth :google_oauth2, ENV['OMNIAUTH_GOOGLE_APP_ID'], ENV['OMNIAUTH_GOOGLE_APP_SECRET'], {}
-  config.omniauth :github, ENV['OMNIAUTH_GITHUB_CLIENT_ID'], ENV['OMNIAUTH_GITHUB_CLIENT_SECRET'], scope: 'user:email'
+  config.omniauth :github, ENV['OMNIAUTH_GITHUB_CLIENT_ID'], ENV['OMNIAUTH_GITHUB_CLIENT_SECRET'], scope: 'user:email, repo'
 
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
