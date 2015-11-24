2f31e626db71ff1ab80c4509d25533ce4c24c9ab
Miguel Fernandes <it.miguelfernandes@gmail.com>
2015-11-24 10:55:43 +0000

    Change authentication_services param from site to password.

--- test/dummy/config/initializers/woodlock.rb
+++ test/dummy/config/initializers/woodlock.rb
@@ -3,6 +3,6 @@ Woodlock.setup do |config|
   config.site_email = 'test_site_email@mail.com'
   config.site_url = 'www.test_site_url.com'
   config.gravatar_default_url = 'http://www.apiflat.com/no_user.png'
-  config.authentication_services = ['google','facebook','site','github']
+  config.authentication_services = ['google','facebook','password','github']
   config.github_scope = 'user:email, repo'
 end
