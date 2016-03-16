75d805d9ebb689c943eb3d90d557793ea491bcf5
Miguel Fernandes <it.miguelfernandes@gmail.com>
2016-03-16 10:29:15 +0000

    Fix mailer provider name.

--- app/mailers/woodlock_welcome_mailer.rb
+++ app/mailers/woodlock_welcome_mailer.rb
@@ -1,10 +1,13 @@
 class WoodlockWelcomeMailer < ApplicationMailer
   def omniauth_welcome(user, provider)
-    case provider
-    when 'google_oauth2' then provider_name = 'Google'
-    when 'facebook' then provider_name = 'Facebook'
-    else provider_name = 'unknown provider'
-    end
+    provider_name =
+      if provider == 'google_oauth2'
+        provider_name = 'Google'
+      elsif provider.blank?
+        'sevice provider'
+      else
+        provider.capitalize
+      end
 
     @greeting = "#{ t('hi') } #{ user.first_name }! #{ t('thanks_for_registration') }."
 
